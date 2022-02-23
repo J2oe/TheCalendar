fe3caca4c9b135be5ed8a6e7e4e77c4578b07d72
Joe <sheninter@163.com>
2022-02-23 19:22:36 +0800

    Initial Commit

---
+++ TheCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheCalendar
+//
+//  Created by Joe on 2022/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheCalendar/TheCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCalendarApp.swift
+//  TheCalendar
+//
+//  Created by Joe on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TheCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
